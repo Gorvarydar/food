64c67692256eb9933467f81166889c4b7457c928
Илья <bothisav@gmail.com>
2021-04-24 17:17:51 +0300

    food 01

---
+++ js/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');  //*css-класс
+            item.classList.remove('show','fade')  //*css-классы
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent (i = 0) {    //* значение по умолчанию при вызове ф -ии без аргумента присваивается 0 если задать это при её определениии
+        tabsContent[i].classList.add('show','fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+
+    }
+
+    hideTabContent();
+    showTabContent(0);
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {         //*  1-й аргумент элемент tabs , 2-й номер этого элемента по порядку
+                if (target == item) {          //* сравнение элемента в который мы кликнули с тем который мы перебираем
+                    hideTabContent();
+                    showTabContent(i);  
+                }
+            })
+        };
+    });
+
+
+
+
+});
