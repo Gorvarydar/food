6e26e1ce85551fe6e121f8178e0d8c1dfe6facc2
Илья <bothisav@gmail.com>
2021-04-24 21:44:57 +0300

    foood

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
-      tabsParent = document.querySelector('.tabs__items');
+      tabsParent = document.querySelector('.tabheader__items');
 
       function hideTabContent () {
           tabsContent.forEach ( item => {
-              item.style.display = "none";
+              item.classList.add('hide');
+              item.classList.remove('show');
           });
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
@@ -14,7 +15,8 @@ const tabs = document.querySelectorAll('.tabheader__item'),
       }
 
       function showTabContent (i = 0) {
-          tabsContent[i].style.display = 'block';
+          tabsContent[i].classList.add('show','fade');
+          tabsContent[i].classList.remove('hide');
           tabs[i].classList.add('tabheader__item_active');
       }
 
@@ -33,41 +35,7 @@ const tabs = document.querySelectorAll('.tabheader__item'),
               });
           }
       });
-
-
-
-    
-
-
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // window.addEventListener('DOMContentLoaded', () => {
 //     const tabs = document.querySelectorAll('.tabheader__item'),
